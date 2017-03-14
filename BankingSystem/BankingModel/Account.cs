24f1473bb74c5ae6abc1489a841d7af9c3d80ce5
Jamile Villarroel Yabar <Jamile Villarroel Yabar>
2017-03-13 21:22:01 -0500

    hola

---
+++ BankingSystem/BankingModel/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingModel
+{
+    public class Account
+    {
+        private string _name;
+
+        //public string getName()
+        //{
+        //    return _name;
+        //}
+
+        //public void setName( string name)
+        //{
+        //    _name = name;
+        //}
+
+        //public Account(string name)
+        //{
+        //    _name = name;
+        //}
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        
+
+        public Account()
+        {
+
+        }
+    }
+}
